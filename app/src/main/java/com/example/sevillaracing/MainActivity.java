11a2118b0b83447657f01cdd0d0324db1efb4ea9
ManuelNaval <KyrieIrving2>
2019-04-12 10:01:22 +0200

    v0.1

--- app/src/main/java/com/example/sevillaracing/MainActivity.java
+++ app/src/main/java/com/example/sevillaracing/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.sevillaracing;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,12 +22,16 @@ public class MainActivity extends AppCompatActivity {
         vv.setVideoURI(uri);
         vv.start();
 
-
         vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
                 mediaPlayer.setLooping(true);
             }
         });
+    }
+
+    public void Registrar(View view) {
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
     }
 }
