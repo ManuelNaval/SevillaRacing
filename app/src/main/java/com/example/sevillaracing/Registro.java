604b269d486b3d314d709ec6c50b0a35c4f31c5f
ManuelNaval <KyrieIrving2>
2019-04-12 11:02:25 +0200

    v1.3

--- app/src/main/java/com/example/sevillaracing/Registro.java
+++ app/src/main/java/com/example/sevillaracing/Registro.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
 
     private EditText nombre, apellidos, email;
+    private CheckBox cb1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class Registro extends AppCompatActivity {
         nombre = (EditText) findViewById(R.id.nombre);
         apellidos = (EditText) findViewById(R.id.apellidos);
         email = (EditText) findViewById(R.id.email);
+        cb1 = (CheckBox) findViewById(R.id.politica);
     }
 
     public void Registrar(View view) {
@@ -29,6 +32,8 @@ public class Registro extends AppCompatActivity {
                 Toast.makeText(this, "Debe introducir sus apellidos", Toast.LENGTH_SHORT).show();
             if (email.length() == 0)
                 Toast.makeText(this, "Debe introducir un email", Toast.LENGTH_SHORT).show();
+        } else if (!cb1.isChecked()) {
+            Toast.makeText(this, "Debe aceptar la política de privacidad", Toast.LENGTH_SHORT).show();
         } else {
             Intent i = new Intent(this, registrocompleto.class);
             i.putExtra("dato", nombre.getText().toString());
